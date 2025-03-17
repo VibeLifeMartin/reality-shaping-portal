3f55ad5b4645e1db31496f3b55fce23137e66a73
Martin Riseng <martin_ris@hotmail.com>
2025-03-17 23:40:04 +0100

    Added homepage

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <h1>Welcome to Reality Shaping Portal</h1>
+        <p>Your journey begins here...</p>
+      </div>
+    );
+  }
+  
