845f94da6b2686832acaa96dd8a35d793a720c17
Martin Riseng <martin_ris@hotmail.com>
2025-03-17 23:52:56 +0100

    Fix: Added required root layout

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <head>
-          <title>Reality Shaping Portal</title>
-        </head>
         <body>
           {children}
         </body>
